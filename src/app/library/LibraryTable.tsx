30c6ea8af03b0276a6579b19879f36251bac8ef8
Tibet Sprague <tibet.sprague@gmail.com>
2024-06-30 14:22:11 -0700

    Red/green/yellow compatability scores
    And Hide filter for now until its more useful

--- src/app/library/LibraryTable.tsx
+++ src/app/library/LibraryTable.tsx
@@ -42,7 +42,7 @@ const LibraryTable: React.FC<LibraryTableProps> = ({ type, items }) => {
 
   return (
     <div>
-      <div className='mb-4 md:mb-0'>Filter: <input name='globalFilter' value={globalFilter} onChange={handleFilterChange} /> </div>
+      {/* Hide until more useful <div className='mb-4 md:mb-0'>Search by name: <input name='globalFilter' value={globalFilter} onChange={handleFilterChange} /> </div> */}
       <table className={styles.libraryTable}>
         <thead>
           <tr>
@@ -54,55 +54,64 @@ const LibraryTable: React.FC<LibraryTableProps> = ({ type, items }) => {
           </tr>
         </thead>
         <tbody>
-          {filteredItems.map((item : Item) => (
-            <tr key={item.name} className='bg-transparent mb-2' style={{ opacity: selectedGoals.length > 0 ? Math.max(item.goalScore || 0, 0.3) : 1}}>
-              {/* data-label used to transform table into list on mobile */}
+          {filteredItems.map((item : Item) => {
+            const compatabilityColor = item.goalScore ? `bg-${item.goalScore >= 0.8 ? 'green' : item.goalScore >= 0.5 ? 'yellow' : 'red'}-400` : 'bg-gray-200';
 
-              <td className='text-center' data-label="">
-                <Link href={`/library/${type}/${item.id}`}>
-                  <h3 className='text-xl font-bold'>{item.name}</h3>
-                </Link>
-              </td>
+            return (
+              <tr key={item.name} className='bg-transparent mb-2' style={{ opacity: selectedGoals.length > 0 ? Math.max(item.goalScore || 0, 0.3) : 1}}>
+                {/* data-label used to transform table into list on mobile */}
 
-              <td data-label="Compatible Entity Structures: ">
-                {item.entityTypes?.map(entity => (
-                  <Link
-                    href={`/library/entity-types/${entity.id}`}
-                    className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block entity-type'
-                    key={entity.name}
-                  >
-                    {entity.name}
+                <td className='text-center' data-label="">
+                  <Link href={`/library/${type}/${item.id}`}>
+                    <h3 className='text-xl font-bold'>{item.name}</h3>
                   </Link>
-                ))}
-              </td>
+                </td>
 
-              <td data-label="Compatible Business Models">
-                {item.businessModels?.map(model => (
-                  <Link
-                    href={`/library/business-models/${model.id}`}
-                    className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block business-model'
-                    key={model.name}
-                  >
-                    {model.name}
-                  </Link>
-                ))}
-              </td>
+                <td data-label="Compatible Entity Structures: ">
+                  {item.entityTypes?.map(entity => (
+                    <Link
+                      href={`/library/entity-types/${entity.id}`}
+                      className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block entity-type'
+                      key={entity.name}
+                    >
+                      {entity.name}
+                    </Link>
+                  ))}
+                </td>
 
-              <td data-label="Compatible Funding Options">
-                {item.fundingOptions?.map(option => (
-                  <Link
-                    href={`/library/funding-options/${option.id}`}
-                    className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block funding-option'
-                    key={option.name}
-                  >
-                    {option.name}
-                  </Link>
-                ))}
-              </td>
+                <td data-label="Compatible Business Models">
+                  {item.businessModels?.map(model => (
+                    <Link
+                      href={`/library/business-models/${model.id}`}
+                      className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block business-model'
+                      key={model.name}
+                    >
+                      {model.name}
+                    </Link>
+                  ))}
+                </td>
 
-              {selectedGoals.length > 0 ? <td data-label="Goal Compatibility"><span className='pl-5'>{Math.round((item.goalScore || 0)  * 100)}</span></td> : null}
-            </tr>
-          ))}
+                <td data-label="Compatible Funding Options">
+                  {item.fundingOptions?.map(option => (
+                    <Link
+                      href={`/library/funding-options/${option.id}`}
+                      className='rounded-2xl text-center px-4 py-1 mr-3 my-1 inline-block funding-option'
+                      key={option.name}
+                    >
+                      {option.name}
+                    </Link>
+                  ))}
+                </td>
+
+                {selectedGoals.length > 0 ?
+                  <td data-label="Goal Compatibility" className='text-center'>
+                    <span className={`pl-5 rounded-2xl w-8 h-8 shadow-md ${compatabilityColor}`} title={"Compatability Score: " + Math.round((item.goalScore || 0)  * 100).toString()}></span>
+                  </td>
+                  : null}
+              </tr>
+            )
+          }
+          )}
         </tbody>
       </table>
     </div>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  safelist: [
+    'bg-green-400',
+    'bg-red-400',
+    'bg-yellow-400'
+  ],
   theme: {
     extend: {
       backgroundImage: {
