d4556bd3d8da5515ab7776c1df77618cd7788aa1
Turquoise Sound <104245594+TurquoiseSound@users.noreply.github.com>
2025-06-04 17:45:55 -0700

    Restore previous changes and improvements

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  swcMinify: false,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
