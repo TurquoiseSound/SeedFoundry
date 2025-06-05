afbb0d4beb4ebaf36af2c1de7a16d5a1e1b8434b
Turquoise Sound <104245594+TurquoiseSound@users.noreply.github.com>
2025-06-05 12:24:41 -0700

    Fix goals select component

--- src/components/GoalsSelect/index.tsx
+++ src/components/GoalsSelect/index.tsx
@@ -1,23 +1,10 @@
-'use client';
-
 import React from 'react';
 
+import { mockGoals } from '@/lib/mockData';
 import { type Goal } from '@/types';
 
 import { default as GoalsSelectInternal } from './GoalsSelect';
 
 export default function GoalsSelect() {
-  const [goals, setGoals] = React.useState<Goal[]>([]);
-
-  React.useEffect(() => {
-    const fetchGoalsData = async () => {
-      const response = await fetch('/api/fetchGoals');
-      const data = await response.json();
-      setGoals(data);
-    };
-    
-    fetchGoalsData();
-  }, []);
-
-  return <GoalsSelectInternal goals={goals} />;
+  return <GoalsSelectInternal goals={mockGoals} />;
 }
